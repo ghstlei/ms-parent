e69a56cad44d52bc10c93c6e3c7adb50b225e2fa
cih <139817646+ghstlei@users.noreply.github.com>
2023-10-21 16:30:06 -0300

    Delete employee-leave-service/employee-leave-service directory

--- employee-leave-service/employee-leave-service/src/main/java/com/microservice/employeeleaveservice/repository/LeaveTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.microservice.employeeleaveservice.repository;
-
-import com.microservice.employeeleaveservice.model.LeaveType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeaveTypeRepository extends JpaRepository<LeaveType, Integer> {}
--- employee-leave-service/employee-leave-service/src/main/java/com/microservice/employeeleaveservice/repository/TypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.microservice.employeeleaveservice.repository;
-
-import com.microservice.employeeleaveservice.model.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<Type, Integer> {}
